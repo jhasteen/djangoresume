0524ace696e298a5a916ecd8b863988ee0a536a7
jhasteen <jacquelinehasteen@gmail.com>
2019-11-25 20:17:59 +0100

    whitenoise typo

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -119,6 +119,7 @@ STATICFILES_DIRS = [
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Configure Django App for Heroku.
 
 django_heroku.settings(locals())
