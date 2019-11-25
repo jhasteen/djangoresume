80f3de7be418714a98955b80ebcabc6ddeed026b
jhasteen <jacquelinehasteen@gmail.com>
2019-11-25 20:00:48 +0100

    added django-heroku dependency

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -1,7 +1,7 @@
 import os
 import django_heroku
-from decouple import config
-import dj_database_url
+# from decouple import config
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,3 +115,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Configure Django App for Heroku.
+
+django_heroku.settings(locals())
