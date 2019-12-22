488c8c18d74617ec1b6dd3cf1d7a7b726e378ba1
jhasteen <jacquelinehasteen@gmail.com>
2019-12-22 15:17:13 +0100

    copypaste from template

--- projects/views.py
+++ projects/views.py
@@ -3,16 +3,10 @@ from projects.models import Project
 
 def project_index(request):
     projects = Project.objects.all()
-    context = {
-        'projects': projects
-    }
-    return render(request, 'project_index.html', context)
+    context = {"projects": projects}
+    return render(request, "project_index.html", context)
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
-    context = {
-        'project': project
-    }
-    return render(request, 'project_detail.html', context)
-
-# Create your views here.
+    context = {"project": project}
+    return render(request, "project_detail.html", context)
